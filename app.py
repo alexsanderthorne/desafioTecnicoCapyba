f1e9b5d31042ff4c4e91bb92cf5713d3bf54ab5b
alexsanderthorne <alexsanderthorne@gmail.com>
2025-04-23 00:44:01 -0300

    Preparando deploy no Render

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
-with app.app_context():
-    print("Rotas registradas:")
-    for rule in app.url_map.iter_rules():
-        print(f"{rule.endpoint}: {rule.methods} -> {rule}")
+
+# with app.app_context():
+#     print("Rotas registradas:")
+#     for rule in app.url_map.iter_rules():
+#         print(f"{rule.endpoint}: {rule.methods} -> {rule}")
